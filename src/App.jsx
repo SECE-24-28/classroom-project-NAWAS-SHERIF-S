d678e5d1ee724c3ca9b56b9f99f2a42719b41cba
NAWAS-SHERIF-S <nawassherif.s2024cse@sece.ac.in>
2025-12-03 19:48:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
+import GetinTouchComponent from "./get-in-touch/get-in-touch-component";
+import GetInTouchComponent from "./get-in-touch/get-in-touch-component";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      {/* <TrustedbyComponent /> */}
+      <GetInTouchComponent/>
+    </div>
+  );
+}
+
+export default App;
